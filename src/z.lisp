7ee072d7b7e520c1516478319b7d30c2fb2f7f2c
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-02-23 10:28:46 +0300

    Do not use SQRT in FACTOR

--- src/z.lisp
+++ src/z.lisp
@@ -18,18 +18,17 @@
   "Factor a non-negative integer @c(n) into prime numbers."
   (declare (optimize (speed 3)))
   (if (< n 2) (list n)
-      (labels ((stop (n) (floor (sqrt n)))
-               (%factor (acc n x stop)
+      (labels ((%factor (acc n x stop)
                  (declare (type integer n x stop))
                  (multiple-value-bind (q r)
                      (floor n x)
                    (cond
                      ((> x stop) (cons n acc))
                      ((zerop r)
-                      (%factor (cons x acc) q 2 (stop q)))
+                      (%factor (cons x acc) q 2 (isqrt q)))
                      (t
                       (%factor acc n (1+ x) stop))))))
-        (%factor nil n 2 (stop n)))))
+        (%factor nil n 2 (isqrt n)))))
 
 (sera:-> totient ((integer 0))
          (values (integer 0) &optional))
